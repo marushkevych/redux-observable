80fd028746bd1f8015803367c08ababff6f56906
Andrey Marushkevych <marushkevych@gmail.com>
2017-10-24 23:05:17 -0400

    clear actions with delay

--- src/epics/index.js
+++ src/epics/index.js
@@ -1,10 +1,12 @@
 import {Observable} from 'rxjs'
 import {combineEpics} from 'redux-observable'
+import {LOAD_STORIES, clear} from '../actions'
 
 function loadStoriesEpic(action$) {
-  return action$
-    .do(action => console.log(action))
-    .ignoreElements()
+  return action$.ofType(LOAD_STORIES)
+    .switchMap(() => {
+      return Observable.of(clear()).delay(2000)
+    })
 }
 
 export const rootEpic = combineEpics(loadStoriesEpic)
